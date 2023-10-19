d6333c94fdfcaf9ae0277d8434b8f902db51458b
Pedro Gardim <me@pedrogardim.com>
2023-10-19 16:05:56 +0200

    docs: swagger basic setup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import express from "express";
 import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+
 import filmsRoute from "./routes/filmsRoutes";
 import usersRoute from "./routes/usersRoutes";
+
+import swaggerDocs from "./config/swagger.json";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocs, { explorer: true })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
