1e6cf485acd94202f3b5dc6514d3f7007f5cf2cc
Pedro Gardim <me@pedrogardim.com>
2023-10-19 12:10:00 +0200

    feat: add usersRoute

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import filmsRoute from "./routes/filmsRoutes";
+import usersRoute from "./routes/usersRoutes";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/films", filmsRoute);
+app.use("/users", usersRoute);
 
 app.listen(PORT, () => {
   console.log(`App listening on ${PORT}`);
