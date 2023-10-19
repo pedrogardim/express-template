88add46c0fdb8f8c1286f0855b872390e207201c
Pedro Gardim <me@pedrogardim.com>
2023-10-19 09:56:19 +0200

    feat: add read port from env

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import filmsRoute from "./routes/filmsRoutes";
 
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
@@ -10,6 +10,6 @@ app.use(morgan("dev"));
 
 app.use("/films", filmsRoute);
 
-app.listen(port, () => {
-  console.log(`App listening on ${port}`);
+app.listen(PORT, () => {
+  console.log(`App listening on ${PORT}`);
 });
