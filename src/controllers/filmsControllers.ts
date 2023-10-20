366b80d3c2ecae24b1aceeb1ad736b5c3c647276
Pedro Gardim <me@pedrogardim.com>
2023-10-20 13:18:04 +0200

    feat(typeorm): add get / post db actions

--- src/controllers/filmsControllers.ts
+++ src/controllers/filmsControllers.ts
@@ -1,4 +1,5 @@
 import { RequestHandler } from "express";
+import { Film } from "../models/Film";
 
 const randomValue = () => Math.floor(Math.random() * 1e6 + 1e7);
 const randomDate = () => new Date(Date.now() * Math.random());
@@ -11,29 +12,43 @@ let films = ["Lord of the Rings", "The Hobbit", "Harry Potter"].map(
   })
 );
 
-export const getAllFilms: RequestHandler = (req, res) => {
-  res.send(films);
+export const getAllFilms: RequestHandler = async (req, res) => {
+  try {
+    const films = await Film.find({ take: 10 });
+    res.send(films);
+  } catch (err) {
+    res.send(err);
+  }
 };
 
-export const getFilmById: RequestHandler = (req, res) => {
-  res.send(films[parseInt(req.params.index)]);
+export const getFilmById: RequestHandler = async (req, res) => {
+  try {
+    const films = await Film.findOne({
+      where: { id: parseInt(req.params.id) },
+    });
+    res.send(films);
+  } catch (err) {
+    res.send(err);
+  }
 };
 
-export const createFilm: RequestHandler = (req, res) => {
-  const newFilm = {
-    ...req.body,
-    views: randomValue(),
-    releaseDate: randomDate(),
-  };
-  /*  #swagger.parameters['body'] = {
+export const createFilm: RequestHandler = async (req, res) => {
+  try {
+    const createdFilm = await Film.create(req.body).save();
+    /*  #swagger.parameters['body'] = {
             in: 'body',
             required: true,
             schema: {
-                title:"Dune"
+                "title": "Dune",
+                "producer": "Jon Mac",
+                "director": "Jon Doe",
+                "release_year": "2020-10-20T10:57:33.815Z"
             }
     } */
-  films.push(newFilm);
-  res.send(films);
+    res.send(createdFilm);
+  } catch (err) {
+    res.send(err);
+  }
 };
 
 export const updateFilm: RequestHandler = (req, res) => {
