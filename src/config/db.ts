65f4999273cddb8cfb86a22ee69312c7b421c3f6
Pedro Gardim <me@pedrogardim.com>
2023-10-23 11:55:56 +0200

    feat(typeorm): create add user fields migration

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,6 +4,7 @@ import { CreateFilms1697789879993 } from "../migration/1697789879993-create-film
 import { CreateUsers1697807671336 } from "../migration/1697807671336-create-users";
 import { CreateGenres1698047081764 } from "../migration/1698047081764-create-genres";
 import { CreateFilmGenre1698048067802 } from "../migration/1698048067802-create-film-genre";
+import { AddUserFields1698050465772 } from "../migration/1698050465772-add-user-fields";
 import { Film } from "../models/Film";
 import { User } from "../models/User";
 
@@ -22,5 +23,6 @@ export const AppDataSource = new DataSource({
     CreateUsers1697807671336,
     CreateGenres1698047081764,
     CreateFilmGenre1698048067802,
+    AddUserFields1698050465772,
   ],
 });
